2e25e2090bcdcb778bf10a9a42d060e4bbc4e69a
Jonathan Harris <jharris319@gmail.com>
2014-03-27 22:57:46 -0500

    eog qgraph

---
+++ qgraph-nix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# a script for quick output of a graphviz dot graph.
+
+if [ $1 ] && [ -f ${1}.dot ] ; then
+	dot -Tpng ${1}.dot -o ${1}.png
+	eog ${1}.png&
+	exit 0
+else
+	exit 1
+fi
